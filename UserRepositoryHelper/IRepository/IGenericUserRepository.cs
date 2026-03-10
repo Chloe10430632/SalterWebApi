ff5e13d618c97ecff9a2ffd01b231045d50020c6
ericlin1308-lgtm <eric.lin1308@gmail.com>
2026-03-10 16:22:51 +0800

    feat 新增部分IGenericUserRepository內容

---
+++ UserRepositoryHelper/IRepository/IGenericUserRepository.cs
@@ -0,0 +1,28 @@
+﻿using SalterEFModels.EFModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRepositoryHelper.IRepository
+{
+    internal interface IGenericUserRepository<Table> where Table : class
+    {
+        Task<SalterDbContext> GetDbContextAsync();
+
+        Task<IEnumerable<Table>> GetAllAsync();
+
+        Task<IEnumerable<Table>> GetByUserId(int id);
+
+        Task CreateAsync(Table entity);
+
+        Task UpdateAsync(Table entity);
+
+        Task DeleteAsync(Table entity);
+
+        Task<bool> SaveChangesAsync();
+
+
+    }
+}
