b9f756e13bf32a9c0265098bc7436eebf54ba727
melody803 <melody803@gmail.com>
2026-03-11 09:31:07 +0800

    CoachDistrict-R

--- ExpRepositoryHelper/CoachIndexRepository.cs
+++ ExpRepositoryHelper/CoachIndexRepository.cs
@@ -21,11 +21,14 @@ namespace ExpRepositoryHelper
 
         public async Task<string> CoachDistrict(int coachID)
         {
-            //return await _dbContext.ExpCoaches
-            //    .Where(c => c.Id == coachID)
-            //    .Select(c => c.TripDistricts)
-            //    .FirstOrDefaultAsync();
-                
+            var districts = await _dbContext.ExpCoaches
+                            .Where(c => c.Id == coachID)
+                            .SelectMany(c => c.TripDistricts) // 展開集合
+                            .Select(d => d.Name)      // 只取名稱
+                            .ToListAsync();                   // 先轉成 List
+
+            // 將 List<string> 合併為單一字串，以逗號隔開
+            return string.Join(", ", districts);
         }
 
         public Task<string> CoachSpeciality()
