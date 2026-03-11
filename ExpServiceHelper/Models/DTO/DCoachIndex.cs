266247f8ab82569948349abd5056e30d0f9f766a
melody803 <melody803@gmail.com>
2026-03-11 20:46:28 +0800

    CoachIndex DTO

---
+++ ExpServiceHelper/Models/DTO/DCoachIndex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpServiceHelper.Models.DTO
+{
+    public class DCoachIndex
+    {
+        public class FavCoachCreateDto
+        {
+            public int UserId { get; set; }
+            public int CoachId { get; set; }
+        }
+
+    }
+}
