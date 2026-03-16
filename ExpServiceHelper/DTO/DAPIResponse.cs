88a2d098e36432cf28adc992934956a3e030d600
melody803 <melody803@gmail.com>
2026-03-16 12:33:01 +0800

    DAPIResponse<T>-DTO

---
+++ ExpServiceHelper/DTO/DAPIResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpServiceHelper.DTO
+{
+    public class DAPIResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+    }
+
+}
