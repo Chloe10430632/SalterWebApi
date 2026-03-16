c220209a729df9c24c28fc904d37c4807344019b
melody803 <melody803@gmail.com>
2026-03-16 12:33:56 +0800

    DEditCoachInfo-DTO

---
+++ ExpServiceHelper/DTO/DEditCoachInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpServiceHelper.DTO
+{
+    public class DEditCoachInfo
+    {
+        //只放讓user異動的欄位
+        public int Id { get; }
+        public string Name { get; set; } = null!;
+        public string? AvatarUrl { get; set; }
+        public string? Introduction { get; set; }
+        public int? DistrictId { get; set; }
+        public int? CityId { get; set; }
+    }
+}
