eb6556eaf6405ead5c2f9d4f99bc11642a1cf0f0
camila13284 <camila132884@gmail.com>
2026-03-17 12:16:09 +0800

    fix: 修改身分驗證邏輯˙

--- TripServiceHelper/Models/DTO/ServiceResult.cs
+++ TripServiceHelper/Models/DTO/ServiceResult.cs
@@ -4,24 +4,26 @@ public class ServiceResult
 {
     public bool IsSuccess { get; set; }
     public string Message { get; set; } = "";
+    public int Code { get; set; } = 200;
 
     public static ServiceResult Success(string message = "成功")
-        => new() { IsSuccess = true, Message = message };
+        => new() { IsSuccess = true, Code = 200, Message = message };
 
-    public static ServiceResult Fail(string message)
-        => new() { IsSuccess = false, Message = message };
+    public static ServiceResult Fail(string message, int code = 400)
+        => new() { IsSuccess = false, Code = code, Message = message };
 }
 
-// 泛型版本 需要回傳資料時使用
-public class ServiceResult<T> 
+//泛型版本 需要回傳資料時使用
+public class ServiceResult<T>
 {
     public bool IsSuccess { get; set; }
     public string Message { get; set; } = "";
+    public int Code { get; set; } = 200;
     public T? Data { get; set; }
 
     public static ServiceResult<T> Success(T data, string message = "成功")
-        => new() { IsSuccess = true, Message = message, Data = data };
+        => new() { IsSuccess = true, Code = 200, Message = message, Data = data };
 
-    public static ServiceResult<T> Fail(string message)
-        => new() { IsSuccess = false, Message = message };
+    public static ServiceResult<T> Fail(string message, int code = 400)
+        => new() { IsSuccess = false, Code = code, Message = message };
 }
