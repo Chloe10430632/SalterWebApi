b769b20545450fd397756cb6a56b96d4be6171f5
Chloe10430632 <a21307096@gmail.com>
2026-03-30 14:54:03 +0800

    feat:新增敏感詞過濾功能

---
+++ ForumServiceHelper/IService/ISensitiveWordsService.cs
@@ -0,0 +1,14 @@
+﻿using ForumServiceHelper.Models.DTO.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumServiceHelper.IService
+{
+    public interface ISensitiveWordsService
+    {
+        public Task<SensitiveWordsResponseModel> ValidateContentAsync(string content);
+    }
+}
---
+++ ForumServiceHelper/Models/DTO/Response/SensitiveWordsResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumServiceHelper.Models.DTO.Response
+{
+    public class SensitiveWordsResponseModel
+    {
+        public bool IsValid { get; set; }
+        public List<string> ViolatedWords { get; set; } = new();
+    }
+}
