89cbd0c2d3cb1bab93723475be8da1b77e5b1673
melody803 <melody803@gmail.com>
2026-04-06 16:11:08 +0800

    mapping

--- SalterEFModels/EFModels/SalterDbContext.cs
+++ SalterEFModels/EFModels/SalterDbContext.cs
@@ -165,9 +165,6 @@ public partial class SalterDbContext : DbContext
 
     public virtual DbSet<UserUserRole> UserUserRoles { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=123.192.123.82,1433;Initial Catalog=Salter;User ID=sa;Password=DX9Qu!3wKWXrbyk;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -551,7 +548,10 @@ public partial class SalterDbContext : DbContext
             entity.Property(e => e.CourseSessionId).HasColumnName("course_session_id");
             entity.Property(e => e.ExpTransactionId).HasColumnName("expTransaction_id");
             entity.Property(e => e.ReservedAt).HasColumnName("reserved_at");
-            entity.Property(e => e.Status).HasColumnName("status");
+            entity.Property(e => e.Status)
+                .HasMaxLength(10)
+                .IsFixedLength()
+                .HasColumnName("status");
             entity.Property(e => e.UpdatedAt).HasColumnName("updated_at");
             entity.Property(e => e.UserId).HasColumnName("user_id");
 
